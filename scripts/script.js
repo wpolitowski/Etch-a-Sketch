06de8555de2f270231c56e2473cefc07593587fb
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-07-28 12:42:56 +0200

    Add event handling for drawing:

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,31 @@
+// GENERATING GRID
 const container = document.querySelector('.container');
 
 function generateGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const div = document.createElement('div');
-        console.log(div)
-        div.classList.add('square');
+        div.classList.add('cell');
         container.appendChild(div);
     }
 }
 
-generateGrid(10)
+generateGrid(16);
+
+
+//CHANGING COLORS
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => cell.addEventListener('mousedown', startSketching));
+
+//change color on hovered elements while the 
+function startSketching() {
+    this.style.backgroundColor = '#ccc'; //change color of a clicked (initial) cell
+    cells.forEach(cell => cell.addEventListener('mouseover', changeColor));
+    cells.forEach(cell => cell.addEventListener('mouseup', () => {
+        cells.forEach(cell => cell.removeEventListener('mouseover', changeColor));
+    }));     
+}
+
+function changeColor() {
+    this.style.backgroundColor = '#ccc'
+}
+
